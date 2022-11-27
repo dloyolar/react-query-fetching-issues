35fe7b01f535c38b4e50cf451b050b3632550a45
dloyolar <dieegolr@gmail.com>
2022-11-27 16:30:21 -0300

    ✨ feat(LabelPicker): get labels from hook

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,23 @@
+import { useLabels } from '../hooks/useLabels';
 
 export const LabelPicker = () => {
+  const labels = useLabels();
+
+  if (labels.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      {labels.data?.map((label) => (
+        <span
+          key={label.id}
+          className="badge rounded-pill m-1 label-picker"
+          style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
         >
-            Primary
+          {label.name}
         </span>
-        
+      ))}
     </div>
-  )
-}
+  );
+};
