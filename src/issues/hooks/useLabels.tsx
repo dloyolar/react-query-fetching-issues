f6bbee5335823c3b08b46333268084ed7a091f10
dloyolar <dieegolr@gmail.com>
2022-11-28 12:16:34 -0300

    🦄 refactor(useLabels): add staleTime

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -10,7 +10,9 @@ const getLabels = async (): Promise<Label[]> => {
 };
 
 export const useLabels = () => {
-  const labelsQuery = useQuery(['labels'], getLabels);
+  const labelsQuery = useQuery(['labels'], getLabels, {
+    staleTime: 1000 * 60 * 60,
+  });
 
   return labelsQuery;
 };
