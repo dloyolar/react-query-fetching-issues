a2430baf765331e34f0d57ff1fd5d24da28e541d
dloyolar <dieegolr@gmail.com>
2022-11-28 12:34:37 -0300

    ✨ feat(ListView): add label selected

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,12 @@
 import { LoadingIcon } from '../../shared/components/LoadingIcon';
 import { useLabels } from '../hooks/useLabels';
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker = ({ selectedLabels, onChange }: Props) => {
   const labels = useLabels();
 
   if (labels.isLoading) {
@@ -13,8 +18,11 @@ export const LabelPicker = () => {
       {labels.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? 'label-active' : ''
+          }`}
           style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -12,6 +12,25 @@ const getLabels = async (): Promise<Label[]> => {
 export const useLabels = () => {
   const labelsQuery = useQuery(['labels'], getLabels, {
     staleTime: 1000 * 60 * 60,
+    // initialData: [],
+    // initialData: [
+    //   {
+    //     id: 791921801,
+    //     node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+    //     name: '❤️',
+    //     color: 'ffffff',
+    //     default: false,
+    //   },
+    //   {
+    //     id: 71502270,
+    //     node_id: 'MDU6TGFiZWw3MTUwMjI3MA==',
+    //     url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Build%20Infrastructure',
+    //     name: 'Component: Build Infrastructure',
+    //     color: 'f9d0c4',
+    //     default: false,
+    //   },
+    // ],
   });
 
   return labelsQuery;
