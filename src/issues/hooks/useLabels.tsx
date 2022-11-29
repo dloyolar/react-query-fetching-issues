18635118eecc0af3e84439ce86a9da05ed258acd
dloyolar <dieegolr@gmail.com>
2022-11-29 12:00:51 -0300

    🦄 refactor(api): add auth token and placeholder

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -5,7 +5,11 @@ import { Label } from '../interfaces/label';
 
 const getLabels = async (): Promise<Label[]> => {
   await sleep(1);
-  const { data } = await githubApi.get<Label[]>('/labels');
+  const { data } = await githubApi.get<Label[]>('/labels', {
+    headers: {
+      Authorization: null,
+    },
+  });
   return data;
 };
 
@@ -13,24 +17,24 @@ export const useLabels = () => {
   const labelsQuery = useQuery(['labels'], getLabels, {
     staleTime: 1000 * 60 * 60,
     // initialData: [],
-    // initialData: [
-    //   {
-    //     id: 791921801,
-    //     node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
-    //     url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
-    //     name: '❤️',
-    //     color: 'ffffff',
-    //     default: false,
-    //   },
-    //   {
-    //     id: 71502270,
-    //     node_id: 'MDU6TGFiZWw3MTUwMjI3MA==',
-    //     url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Build%20Infrastructure',
-    //     name: 'Component: Build Infrastructure',
-    //     color: 'f9d0c4',
-    //     default: false,
-    //   },
-    // ],
+    placeholderData: [
+      {
+        id: 791921801,
+        node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+        url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+        name: '❤️',
+        color: 'ffffff',
+        default: false,
+      },
+      {
+        id: 71502270,
+        node_id: 'MDU6TGFiZWw3MTUwMjI3MA==',
+        url: 'https://api.github.com/repos/facebook/react/labels/Component:%20Build%20Infrastructure',
+        name: 'Component: Build Infrastructure',
+        color: 'f9d0c4',
+        default: false,
+      },
+    ],
   });
 
   return labelsQuery;
