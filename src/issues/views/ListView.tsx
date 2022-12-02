75e47ec24a519b0c738135ac845105b7643602b3
dloyolar <dieegolr@gmail.com>
2022-12-02 11:15:19 -0300

    ✨ feat(ListView): add state for status and labels

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -3,10 +3,12 @@ import { LoadingIcon } from '../../shared/components/LoadingIcon';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
 import { useIssues } from '../hooks';
+import { State } from '../interfaces';
 
 export const ListView = () => {
+  const [state, setState] = useState<State>();
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-  const { issuesQuery } = useIssues();
+  const { issuesQuery } = useIssues({ state, labels: selectedLabels });
 
   const onLabelChange = (labelName: string) => {
     selectedLabels.includes(labelName)
@@ -17,7 +19,15 @@ export const ListView = () => {
   return (
     <div className="row mt-5">
       <div className="col-8">
-        {issuesQuery.isLoading ? <LoadingIcon /> : <IssueList issues={issuesQuery.data || []} />}
+        {issuesQuery.isLoading ? (
+          <LoadingIcon />
+        ) : (
+          <IssueList
+            issues={issuesQuery.data || []}
+            state={state}
+            onStateChanged={(newState) => setState(newState)}
+          />
+        )}
       </div>
 
       <div className="col-4">
